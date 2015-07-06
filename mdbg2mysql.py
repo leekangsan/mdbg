4a3cd872252e8ff6795af33c8c058411072ace91
m.cherkasov <maxim.cherkasov@gmail.com>
2015-07-06 18:49:51 +0300

    Small fix

--- mdbg2mysql.py
+++ mdbg2mysql.py
@@ -7,14 +7,18 @@ try:
     print("filename: ", file_name)
     f = open(file_name)
     fw = open('chinese_dictionary.sql', 'w')
+    i = 0
     for line in f:
         if line[0] != '#':
             str1 = line.split(" ")
             # print(str1[0], " ", str1[1], " ", line.partition('[')[-1].partition(']')[0])
-            sql = "INSERT INTO study_platform.chinese_dictionary (traditional, simplified, pinyin) VALUES ("\
-                  +"'"+str1[0]+"',"+"'"+str1[1]+"',"\
-                  +"'"+line.partition('[')[-1].partition(']')[0]+"');"
-            fw.write(sql+'\n')
+            sql = "INSERT INTO study_platform.chinese_dictionary (traditional, simplified, pinyin) VALUES (" \
+                  + "'" + str1[0] + "'," + "'" + str1[1] + "'," \
+                  + "'" + line.partition('[')[-1].partition(']')[0] + "');"
+            fw.write(sql + '\n')
+            i += 1
+
+    print("Lines: " + i)
 
 except IndexError:
     print("usage: ", sys.argv[0], " <filename>")
